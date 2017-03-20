20d1eaa99d9fa97c966947e94eedb3e24e07971b
davideColangelo <davide.colangelo@edu.itspiemonte.it>
2017-03-20 03:08:50 +0100

    loginCheck.dll

---
+++ loginCheck/loginCheck/Mail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace loginCheck
+{
+    public class Mail
+    {
+        private static Regex createValidEmailRegex()
+        {
+            string validEmailPattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+            return new Regex(validEmailPattern, RegexOptions.IgnoreCase);
+        }
+        public static bool isValidEmail(string emailAddress)
+        {
+            Regex validEmailRegex = createValidEmailRegex();
+            bool isValid = validEmailRegex.IsMatch(emailAddress);
+            return isValid;
+        }
+    }
+}
---
+++ loginCheck/loginCheck/Password.cs
@@ -0,0 +1,37 @@
+﻿using PCLCrypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace loginCheck
+{
+    public class Password
+    {
+        public static string makePassword(string password)
+        {
+            if (password.Length < 8)
+            {
+                return "Error: 8";
+            }
+            else if (!Regex.IsMatch(password, @"^[a-zA-Z0-9_]+$"))
+            {
+                return "Error: symbol";
+            }
+
+            return hash(password);
+
+        }
+        public static string hash(string text)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            var hasher = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha256);
+            byte[] hash = hasher.HashData(data);
+            return Convert.ToBase64String(hash);
+
+        }
+    }
+}
