b93115f936d3eb61a505687b6083812efaff0be8
Davide Colangelo <colangelod@AULE.IT>
2017-03-20 12:44:10 +0100

    loginCheck funzionante e TestDll

---
+++ TestDll/TestDll/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using loginCheck;
+
+namespace TestDll
+{
+    [Activity(Label = "TestDll", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            TextView outLog = FindViewById<TextView>(Resource.Id.textView1);
+            outLog.Text = Password.makePassword("miaPassword123");
+        }
+    }
+}
+
